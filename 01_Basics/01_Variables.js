a402a635e0ca1c309a6d601cbf54fb32af617c66
exceptional007 <akshhatsri7843@gmail.com>
2025-08-03 23:46:56 +0530

    Learn Variables, Datatypes & Conversion

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,21 @@
+const accountID = 144553
+let accountEmail = "akshhatsri@gmail.com"
+var accountPassword = "4534adag"
+accountCity = "Lucknow"
+let accountState;
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+
+//accountID = 1234
+accountEmail = "right@gmail.com"
+accountPassword = "1234abcd"
+accountCity = "Patna"
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+console.log(accountCity)
+
+/* 
+    Prefer not to use var 
+    because of 
+    functional Scope & Block Scope
+*/
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // We are using nodejs, not browser
+
+console.log(3+3)    // Code Readability is importantff
+console.log("Doraemon")
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+
+// PRIMITIVE DATATYPES
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/ false
+// null = standalone value representation of empty value type: object
+// undefined => not empty type: undefineds
+// symbol => Uniqueness 
+
+// OBJECT
+
+console.table([typeof "bheem", typeof null, typeof age, typeof name, typeof undefined, typeof isLoggedIn])
