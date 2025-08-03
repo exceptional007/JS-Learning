185923ca849e74a8231140fada4520f531d48195
exceptional007 <akshhatsri7843@gmail.com>
2025-08-04 00:05:31 +0530

    Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -26,6 +26,38 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
 
+// ************* Operations **************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2)
+
+let str1 = "hello"
+let str2 = " Virat"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + '2');
+// console.log('1' + 2 + 2); // String first hai toh all treated as string
+// console.log(1 + 2 + "2"); // String last me hai toh phele saare operation then conversion
+
+
+// console.log(+false);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+++gameCounter;
+console.log(gameCounter)
