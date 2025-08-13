1c51b7542a3c9ee1334ad90d57a2a590843b3694
exceptional007 <akshhatsri7843@gmail.com>
2025-08-13 22:00:19 +0530

    Setup To-Do App

--- Test.js
+++ Test.js
@@ -1,1 +1,32 @@
-console.log("hello");
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve('Promise 1 resolved'), 500);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject('Promise 2 rejected'), 200);
+});
+
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve('Promise 3 resolved'), 1000);
+});
+
+Promise.race([promise1, promise2, promise3])
+  .then(value => {
+    console.log('Resolved:', value); // This will not be reached in this example
+  })
+  .catch(reason => {
+    console.log('Rejected:', reason); // Output: Rejected: Promise 2 rejected
+  });
+
+// Task 3
+function fetchWithTimeout(promise, timeout) {
+    const timeoutPromise = new Promise((_, reject) => 
+    setTimeout(() => reject('Timeout exceeded'), timeout)
+  );
+  
+  return Promise.race([promise, timeoutPromise]);
+}
+
+function fetchData() {
+  return new Promise(resolve => setTimeout(() => resolve('Data fetched'), 3000));
+}
