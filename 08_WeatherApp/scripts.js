6bb49a124b7f79bbdc4a85f3deb090b48b536e82
exceptional007 <akshhatsri7843@gmail.com>
2025-09-24 00:22:03 +0530

    Add main Functionality

--- 08_WeatherApp/scripts.js
+++ 08_WeatherApp/scripts.js
@@ -7,11 +7,69 @@ document.addEventListener('DOMContentLoaded', () => {
    const descriptionDisplay = document.getElementById("description")
    const errorMessage = document.getElementById("error-message")
    
-   getWeatherBtn.addEventListener('click', () => {
-    const city = cityInput.ariaValueMax.trim()
+   const API_KEY = apiKey
+    
+
+   getWeatherBtn.addEventListener('click', async () => {
+    const city = cityInput.value.trim()
+    
     if (!city) {
         return alert('Enter Seher')
     }
     
+    // it may throw an error
+    // server/ DB is always in another continent
+
+    try {
+        const weatherData = await fetchWeatherData(city)
+        displayWeatherData(weatherData)
+    } catch (error) {
+        console.log(error);
+        
+        showError()
+    }
+    
    })
+
+    async function fetchWeatherData(city) {
+    
+        // gets the data
+
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+        const response = await fetch(url)
+        console.log(typeof response);
+        console.log("Response", response);
+
+        if (!response.ok) {
+            throw new Error("City Not Found !");  
+        }
+        const data = await response.json()
+        return data
+   }
+
+   function displayWeatherData(data) {
+
+        console.log(data);
+        const {name, main, weather} = data
+        cityNameDisplay.textContent = name;
+        let temperature = main.temp-273.15;
+        temperature = parseFloat(temperature.toFixed(2));
+        temperatureDisplay.textContent = `Temperature : ${temperature}\u2103`;
+        let weatherDesc = weather[0].description;
+        weatherDesc = weatherDesc.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+        descriptionDisplay.textContent = `Weather : ${weatherDesc}`;
+    
+
+        // unlock the display
+
+        weatherInfo.classList.remove('hidden');
+        errorMessage.classList.add('hidden');
+
+        
+   }
+
+   function showError() {
+    weatherInfo.classList.add("hidden")
+    errorMessage.classList.remove("hidden")
+   }
 })
